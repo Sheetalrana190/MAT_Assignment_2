99158b2f25efeab662c54f210e57409a50641055
sheetal <srana4226@conestoga.on.ca>
2025-03-12 15:01:47 -0400

    /calculateCanvasSize.test.ts : write a passing test for should throw an error when entered length of Canvas is negative

--- src/calculateCanvasSize/calculateCanvasSize.test.ts
+++ src/calculateCanvasSize/calculateCanvasSize.test.ts
@@ -6,7 +6,9 @@ describe("calculateCanvasSize", () => {
 
     expect(result).toEqual(1000);
   });
-  test.todo("should throw an error when entered length of Canvas is negative");
+  test("should throw an error when entered length of Canvas is negative", () => {
+    expect(() => calculateCanvasSize("-10", "100")).toThrow("Canvas Length should be possitive");
+  });
   test.todo("should throw an error when entered width of Canvas is negative");
   test.todo("should throw an error when entered length of Canvas is zero");
   test.todo("should throw an error when entered width of Canvas is zero");
--- src/calculateCanvasSize/calculateCanvasSize.ts
+++ src/calculateCanvasSize/calculateCanvasSize.ts
@@ -2,5 +2,9 @@ export default function calculateCanvasSize(
   length: string,
   width: string
 ) {
+  if (parseInt(length) < 0){
+    throw new Error("Canvas Length should be possitive");
+  }
   return parseInt(length) * parseInt(width);
+  
 }
