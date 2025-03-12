e4ff7dddb3220eb7894eb307e2678bb86e25f6ec
sheetal <srana4226@conestoga.on.ca>
2025-03-12 15:49:33 -0400

    paintRequiredCalculator.test.ts : write test case for all the unit test together

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -12,11 +12,21 @@ describe("paint required calculator", () => {
   test("should throw an error when entered coveragePerLiter value is negative", () => {
       expect(() => paintRequiredCalculator(50, -10)).toThrow("coveragePerLiter value should be possitive");
     });
-  test.todo("should throw an error when entered area is zero");
-  test.todo("should throw an error when coveragePerLiter value is zero.");
-  test.todo("should give correct result for decimal area");
-  test.todo("should give correct resultt for decimal coveragePerLiter ");
+  test("should throw an error when entered area is zero", () => {
+    expect(() => paintRequiredCalculator(0, 10)).toThrow("area should not be zero");
+  });
+  test("should throw an error when coveragePerLiter value is zero.", () => {
+    expect(() => paintRequiredCalculator(50, 0)).toThrow("coveragePerLiter should not be zero");
+  });
+  test("should give correct result for decimal area", () => {
+    expect(() => paintRequiredCalculator(50.5, 10)).toEqual(5.05);
+  });
+  test("should give correct resultt for decimal coveragePerLiter ", () => {
+    expect(() => paintRequiredCalculator(50, 10.5)).toEqual(4.76);
+  });
   //assume we are going to use only A4 size paper: A4 format size is 625 cm²  -the standard format for personal printers
-  test.todo("should throw an error if the area value go beyond 625 sq cm standard value");
+  test("should throw an error if the area value go beyond 625 sq cm standard value", () => {
+    expect(() => paintRequiredCalculator(626, 10)).toThrow("area should not be more then 625");
+  });
   
 });
--- src/paintRequiredCalculator/paintRequiredCalculator.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.ts
@@ -2,8 +2,19 @@ export default function paintRequiredCalculator(area: number, coveragePerLiter: 
   if (area < 0){
     throw new Error("area should be possitive");
 }
+if (area == 0){
+  throw new Error("area should not be zero");
+}
+if (area > 625){
+  throw new Error("area should not be more then 625");
+}
 if (coveragePerLiter < 0){
   throw new Error("coveragePerLiter value should be possitive");
 }
+
+if (coveragePerLiter == 0){
+  throw new Error("coveragePerLiter should not be zero");
+}
+
   return area / coveragePerLiter;
 }
