36057a4ab7340e1ad43f4fde77f83c1e85dc22a4
sheetal <srana4226@conestoga.on.ca>
2025-03-12 14:49:47 -0400

    paintRequiredCalculator.test.ts : Write the todo list for unit test cases

--- src/paintRequiredCalculator/paintRequiredCalculator.test.ts
+++ src/paintRequiredCalculator/paintRequiredCalculator.test.ts
@@ -6,5 +6,12 @@ describe("paint required calculator", () => {
 
     expect(result).toEqual(5);
   });
+  test.todo("should throw an error when entered area is negative");
+  test.todo("should throw an error when entered coveragePerLiter value is negative");
+  test.todo("should throw an error when coveragePerLiter value is zero.");
+  test.todo("should give correct result for decimal area");
+  test.todo("should give correct resultt for decimal coveragePerLiter ");
+  //assume we are going to use only A4 size paper: A4 format size is 625 cm²  -the standard format for personal printers
+  test.todo("should throw an error if the area value go beyond 625 sq cm standard value");
   
 });
